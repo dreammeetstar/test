52002b4d2541882dc824e59f6dced527738a90d3
梦晨 <47660180+dreammeetstar@users.noreply.github.com>
2023-05-16 21:18:10 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from markupsafe import escape
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Welcome to My Test!"
+    return "<h1>Welcome!</h1>"
+
+@app.route('/user/<name>')
+def user_page(name):
+	return f"<h1>Welcome to My Test!</h1><div>hello!{escape(name)}</div><img src='http://helloflask.com/totoro.gif'>"
+
+@app.route("/test")
+def test():
+    print(url_for('hello'))
+    print(url_for('user_page',name='123'))
+    print(url_for('test'))
+    print(url_for('test',name='12'))
+    return '什么？！！'
